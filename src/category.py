099da09f9996b655a547ee2261d7d4e912f57063
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-01-18 16:53:12 +0700

    tasks 1, 2 done

--- src/category.py
+++ src/category.py
@@ -1,12 +1,14 @@
 from typing import Optional
 
+from src.product import Product
+
 
 class Category:
     """Класс с описанием категории продуктов"""
 
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
@@ -17,6 +19,26 @@ class Category:
         self.description = description
         if products is None:
             products = []
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products(self) -> str:
+        """Геттер приватного атрибута __products,
+        возвращает каждый объект из списка продуктов категории в виде форматированной строки."""
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
+
+    def add_product(self, product: Product) -> None:
+        """Метод добавления продукта в список продуктов категории.
+        Если в категории уже существует добавляемый продукт,
+        то количество существующего объекта увеличивается на значение количества добавляемого объекта."""
+
+        if product not in self.__products:
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            self.__products[self.__products.index(product)].quantity += product.quantity
